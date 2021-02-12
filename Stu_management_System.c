470403d70fbae71bb80918c63c2fca497340db5a
3019244266-Li Runze <Lirz3019244266@163.com>
2021-02-12 09:54:17 +0800

    add a structure of students

--- Stu_management_System.c
+++ Stu_management_System.c
@@ -4,3 +4,13 @@
 #include<stdlib.h>
 #include<string.h>
 
+struct student{     //学生的结构体
+    char code[20];      //学号
+    char name[10];      //姓名
+    int age;            //年龄
+    char sex;           //性别
+    char address[80];   //地址
+    char e_mail[50];    //邮箱
+    char tel[11];       //电话
+    char birth[40];     //出生日期
+}stu[200];
